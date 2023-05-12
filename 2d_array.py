ba32930042ee30c0389ae7b80540714a535acd28
Steve Lehew <120685948+SLehew@users.noreply.github.com>
2023-05-12 11:21:36 -0400

    2d_array reading txt file into x y array working. Now need to finf min values

--- 2d_array.py
+++ 2d_array.py
@@ -1,13 +1,11 @@
-elevation_array = []
 
-with open('elevation_small.txt') as file:
-    main_list = file.readline()
 
-    for line in main_list:
-        point = line.split(' ')
-        x = int(point[0])
-        y = int(point[1])
-        point_as_array = [x, y]
-        elevation_array.append(point_as_array)
+x, y = [], []
 
-print(elevation_array)
+with open('elevation_small.txt') as elevation_file:
+    for line in elevation_file.readlines():
+        row = line.split()
+        x.append(row[0])
+        y.append(row[1])
+
+print(x[1], y[4])
