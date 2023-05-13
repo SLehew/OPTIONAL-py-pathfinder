ae20e1cbc2dd2686ceda3db95be0c0cf885f0ebc
Steve Lehew <120685948+SLehew@users.noreply.github.com>
2023-05-13 10:18:16 -0400

    2d_array working, now need to find min and max elevations

--- 2d_array.py
+++ 2d_array.py
@@ -1,11 +1,8 @@
+elevation_array = []
+
+with open("elevation_small.txt") as elevation_file:
+    lines = elevation_file.readlines()
+elevation_array = [l.strip().split(" ") for l in lines]
 
 
-x, y = [], []
-
-with open('elevation_small.txt') as elevation_file:
-    for line in elevation_file.readlines():
-        row = line.split()
-        x.append(row[0])
-        y.append(row[1])
-
-print(x[1], y[4])
+print()
